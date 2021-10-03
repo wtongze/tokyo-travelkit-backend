14e8d43d0f4802b65af036b7252ad2a0d276b8b2
Tongze Wang <wtongze@gmail.com>
2021-10-03 17:20:30 +0800

    add fare endpoint

--- src/routers/railway.ts
+++ src/routers/railway.ts
@@ -2,6 +2,8 @@ import express from 'express';
 import { Operator } from '../models/operator';
 import { RailDirection } from '../models/railDirection';
 import { Railway } from '../models/railway';
+import { RailwayFare } from '../models/railwayFare';
+import { Station } from '../models/station';
 
 const railwayRouter = express.Router();
 
@@ -37,6 +39,60 @@ railwayRouter.get('/info/:id', async (req, res) => {
     } else {
       res.sendStatus(500);
     }
+  } else {
+    res.sendStatus(404);
+  }
+});
+
+railwayRouter.get('/fare/:from/:to', async (req, res) => {
+  const from = await Station.findByPk(req.params.from);
+  const to = await Station.findByPk(req.params.to);
+  if (from && to) {
+    const railwayFareList = await RailwayFare.findAll({
+      where: {
+        odptFromStation: from.owlSameAs,
+        odptToStation: to.owlSameAs,
+      },
+    });
+    const response: any[] = [];
+    for (const railwayFare of railwayFareList) {
+      const operator = await Operator.findByPk(railwayFare.odptOperator);
+      const viaStation: any[] = [];
+      for (const viaStationId of railwayFare.odptViaStation || []) {
+        const viaStationObj = await Station.findByPk(viaStationId);
+        viaStation.push({
+          id: viaStationId,
+          stationTitle: viaStationObj
+            ? viaStationObj.odptStationTitle || undefined
+            : undefined,
+        });
+      }
+      const viaRailway: any[] = [];
+      for (const viaRailwayId of railwayFare.odptViaRailway || []) {
+        const viaRailwayObj = await Railway.findByPk(viaRailwayId);
+        viaRailway.push({
+          id: viaRailwayId,
+          stationTitle: viaRailwayObj
+            ? viaRailwayObj.odptRailwayTitle || undefined
+            : undefined,
+        });
+      }
+      response.push({
+        id: railwayFare.owlSameAs,
+        operatorTitle: operator ? operator.odptOperatorTitle : undefined,
+        fromStationTitle: from.odptStationTitle || undefined,
+        toStationTitle: to.odptStationTitle || undefined,
+        ticketFare: railwayFare.odptTicketFare,
+        icCardFare: railwayFare.odptIcCardFare || undefined,
+        childTicketFare: railwayFare.odptChildIcCardFare || undefined,
+        childIcCardFare: railwayFare.odptChildIcCardFare || undefined,
+        viaStation: viaStation.length > 0 ? viaStation : undefined,
+        viaRailway: viaRailway.length > 0 ? viaRailway : undefined,
+        ticketType: railwayFare.odptTicketType || undefined,
+        paymentMethod: railwayFare.odptPaymentMethod || undefined,
+      });
+    }
+    res.send(response);
   } else {
     res.sendStatus(404);
   }
